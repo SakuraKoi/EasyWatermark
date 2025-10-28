cc6063d838be40c631c4515214b279be0a9a1909
SakuraKooi <me@sakurakooi.dev>
2025-10-28 15:33:26 +0800

    feat: optimize performance

--- src/main/java/dev/sakurakooi/easywatermark/WatermarkRenderer.java
+++ src/main/java/dev/sakurakooi/easywatermark/WatermarkRenderer.java
@@ -7,78 +7,103 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class WatermarkRenderer {
+    private static final double REFERENCE_DIAGONAL = Math.sqrt(1920.0 * 1920.0 + 1080.0 * 1080.0);
+    private static final RenderingHints[] WATERMARK_RENDERING_HINTS = {
+        new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON),
+        new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON),
+        new RenderingHints(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON)
+    };
+    private static final RenderingHints[] RESULT_RENDERING_HINTS = {
+        new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON),
+        new RenderingHints(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR),
+        new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+    };
+
     public static BufferedImage renderWatermark(BufferedImage processingImage, Configuration configuration) {
-        double imageDiagonal = Math.sqrt(
-            processingImage.getWidth() * processingImage.getWidth() + 
-            processingImage.getHeight() * processingImage.getHeight()
-        );
-        double referenceDiagonal = Math.sqrt(1920.0 * 1920.0 + 1080.0 * 1080.0); // Reference: 1920x1080
-        double scaleFactor = imageDiagonal / referenceDiagonal;
+        if (configuration.getTransparency() == 0) {
+            return processingImage;
+        }
+
+        String watermarkText = configuration.getText();
+        int rotate = configuration.getRotate();
+        int transparency = configuration.getTransparency();
+
+        int imageWidth = processingImage.getWidth();
+        int imageHeight = processingImage.getHeight();
+        double imageDiagonal = Math.sqrt(imageWidth * imageWidth + imageHeight * imageHeight);
+        double scaleFactor = imageDiagonal / REFERENCE_DIAGONAL;
 
         int scaledFontSize = Math.max(4, (int) Math.round(configuration.getFontSize() * scaleFactor));
         int scaledGapX = Math.max(2, (int) Math.round(configuration.getGapX() * scaleFactor));
         int scaledGapY = Math.max(2, (int) Math.round(configuration.getGapY() * scaleFactor));
 
-        int watermarkWidth = processingImage.getWidth() * 2;
-        int watermarkHeight = processingImage.getHeight() * 2;
-        BufferedImage watermarkImage = new BufferedImage(watermarkWidth, watermarkHeight, BufferedImage.TYPE_INT_ARGB);
+        double rotationRadians = Math.abs(Math.toRadians(rotate));
+        int sizeMultiplier = (rotate == 0) ? 1 : (int) Math.ceil(1.0 + Math.sin(rotationRadians) + Math.cos(rotationRadians));
+        int watermarkWidth = imageWidth * sizeMultiplier;
+        int watermarkHeight = imageHeight * sizeMultiplier;
+
+        BufferedImage watermarkImage = new BufferedImage(watermarkWidth, watermarkHeight, BufferedImage.TYPE_INT_ARGB_PRE);
 
         Graphics2D watermarkGraphics = watermarkImage.createGraphics();
 
-        watermarkGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        watermarkGraphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        watermarkGraphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        for (RenderingHints hint : WATERMARK_RENDERING_HINTS) {
+            watermarkGraphics.addRenderingHints(hint);
+        }
 
         Font font = new Font(configuration.getFont(), configuration.getFontStyle(), scaledFontSize);
         watermarkGraphics.setFont(font);
-
         Color textColor = new Color((int) (configuration.getColor() & 0xFFFFFFFFL), true);
         watermarkGraphics.setColor(textColor);
 
         FontMetrics fontMetrics = watermarkGraphics.getFontMetrics();
-        int textWidth = fontMetrics.stringWidth(configuration.getText());
+        int textWidth = fontMetrics.stringWidth(watermarkText);
         int textHeight = fontMetrics.getHeight();
+        int horizontalSpacing = textWidth + scaledGapX;
+        int verticalSpacing = textHeight + scaledGapY;
 
         int row = 0;
-        int horizontalSpacing = textWidth + scaledGapX;
+        int startXOffset = horizontalSpacing / 2;
         
-        for (int y = 0; y < watermarkHeight + textHeight; y += textHeight + scaledGapY) {
-            int startX = (row % 2 == 1) ? (horizontalSpacing / 2) : 0;
+        for (int y = 0; y < watermarkHeight + textHeight; y += verticalSpacing) {
+            int startX = (row % 2 == 1) ? startXOffset : 0;
+
+            int instancesNeeded = (watermarkWidth + textWidth - startX + horizontalSpacing - 1) / horizontalSpacing;
             
-            for (int x = startX; x < watermarkWidth + textWidth; x += horizontalSpacing) {
-                watermarkGraphics.drawString(configuration.getText(), x, y);
+            for (int i = 0; i < instancesNeeded; i++) {
+                int x = startX + i * horizontalSpacing;
+                watermarkGraphics.drawString(watermarkText, x, y);
             }
             row++;
         }
         
         watermarkGraphics.dispose();
 
-        BufferedImage resultImage = new BufferedImage(processingImage.getWidth(), processingImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        BufferedImage resultImage = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB_PRE);
         Graphics2D resultGraphics = resultImage.createGraphics();
 
-        resultGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        resultGraphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        resultGraphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        for (RenderingHints hint : RESULT_RENDERING_HINTS) {
+            resultGraphics.addRenderingHints(hint);
+        }
 
         resultGraphics.drawImage(processingImage, 0, 0, null);
 
-        if (configuration.getRotate() != 0) {
-            double centerX = processingImage.getWidth() / 2.0;
-            double centerY = processingImage.getHeight() / 2.0;
+        if (rotate != 0) {
+            double centerX = imageWidth / 2.0;
+            double centerY = imageHeight / 2.0;
 
             AffineTransform transform = new AffineTransform();
             transform.translate(centerX, centerY);
-            transform.rotate(Math.toRadians(configuration.getRotate()));
+            transform.rotate(Math.toRadians(rotate));
             transform.translate(-centerX, -centerY);
             
             resultGraphics.setTransform(transform);
         }
 
-        float alpha = configuration.getTransparency() / 255.0f;
+        float alpha = transparency / 255.0f;
         resultGraphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-        
-        int drawX = -(watermarkWidth - processingImage.getWidth()) / 2;
-        int drawY = -(watermarkHeight - processingImage.getHeight()) / 2;
+
+        int drawX = -(watermarkWidth - imageWidth) / 2;
+        int drawY = -(watermarkHeight - imageHeight) / 2;
         resultGraphics.drawImage(watermarkImage, drawX, drawY, null);
         
         resultGraphics.dispose();
